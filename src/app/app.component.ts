d2b87ec3ad9c82180f244286b75c2d538451558a
Shraddha Shelke <sshelke@internal.tavisca.com>
2020-09-21 21:13:15 +0530

    sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'movieShare';
+  title = 'movieSharejkhjbjkkb';
   name = 'Angular and LitElement';
 }
